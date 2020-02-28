1cc57c243d04229b42d71f44eba69762928060a7
Mathieu De Jesus <mathieu.de-jesus@ens.uvsq.fr>
2020-02-28 12:44:24 +0100

    Création class qui calcule le salaire

---
+++ pglp_3_1/src/main/java/Mathieu_De_Jesus/pglp_3_1/Employe1.java
@@ -0,0 +1,18 @@
+package Mathieu_De_Jesus.pglp_3_1;
+
+class Employe1 {
+	
+	@SuppressWarnings("unused")
+	private String name;
+	@SuppressWarnings("unused")
+	private String adresse;
+	
+	public Employe1(String name, String adresse) {
+		this.name = name;
+		this.adresse = adresse;
+	}
+	
+	public double calculSalaire() {
+		return 1500.56;
+	}
+}
